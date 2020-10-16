d83b958112e80ac81c3c0169846daed4c5ea5f63
Peter Marra <neverquit@roadrunner.com>
2020-10-16 15:59:07 -0400

    initial load

---
+++ DIP-Demo/GainDivergenceChecker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using System.Text;
+
+namespace SOLID.DIP
+{
+    public class GainDivergenceChecker
+    {
+        //private Accounter _privateAccounter;
+        //private FiscalRegistrator _fr;
+        private IAccounter _privateAccounter;
+        private IFiscalRegistrator _fr;
+
+        public GainDivergenceChecker(IAccounter accounter,IFiscalRegistrator fr)
+        {
+            //    _privateAccounter = new Accounter();
+            //    _fr = new FiscalRegistrator();
+                _privateAccounter = accounter;
+                _fr = fr;
+        }
+
+        public bool HasDivergence()
+        {
+            decimal salesSumm = _privateAccounter.GetSalesSumm();
+            decimal summOfRetuenedTicket = _privateAccounter.GetSummOfReturnedTickets();
+
+            decimal salesSummByFiscalRegistor = _fr.GetSalesSumm();
+            decimal summOfReturnedTicketsByFiscalRegistrator = _fr.GetSummOfReturnedTickets();
+
+            return salesSumm == salesSummByFiscalRegistor &&
+                   summOfRetuenedTicket == summOfReturnedTicketsByFiscalRegistrator;
+        }
+    }
+
+    public interface IAccounter
+    {
+        decimal GetSalesSumm();
+        decimal GetSummOfReturnedTickets();
+    }
+
+    public class Accounter: IAccounter
+    {
+        public  decimal GetSalesSumm()
+        {
+            return 0;
+        }
+
+        public decimal GetSummOfReturnedTickets()
+        {
+            return 0;
+        }
+    }
+
+    public interface IFiscalRegistrator
+    {
+        public decimal GetSalesSumm();
+        public decimal GetSummOfReturnedTickets();
+    }
+    public class FiscalRegistrator: IFiscalRegistrator
+    {
+        public decimal GetSalesSumm()
+        {
+            return 0;
+        }
+
+        public decimal GetSummOfReturnedTickets()
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ DIP-Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLI.DDIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
